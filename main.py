b5b14df626615c9feb657a77ce508cb0b0813afd
Vyadavgit <70733761+Vyadavgit@users.noreply.github.com>
2024-10-20 19:23:44 -0500

    troubleshoot IR sensor

--- main.py
+++ main.py
@@ -6,19 +6,15 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 # IR-08H Avoid Sensor (EN_IR, OUT_IR, PWR_IR, GND_IR)
-OUT_IR = 16
+OUT_IR = 33
 EN_IR = 18
 GPIO.setup(OUT_IR, GPIO.IN)
 GPIO.setup(EN_IR, GPIO.OUT)
-# GPIO.output(EN_IR, GPIO.HIGH)
 print("IR Sensor Ready.....")
 print(" ")
 
-# GPIO.output(EN_IR, GPIO.HIGH)
-
 try: 
     while True:
-        # print("enable pin: ",GPIO.input(EN_IR))
         if(GPIO.input(OUT_IR)):
             print("Obstacle detected")
         else:
