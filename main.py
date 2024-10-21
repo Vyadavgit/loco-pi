c9f81c6a41fa55fc21646834f05c38fc6e1cb449
Vyadavgit <70733761+Vyadavgit@users.noreply.github.com>
2024-10-20 19:32:06 -0500

    troubleshoot IR sensor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import RPi.GPIO as GPIO
 from time import sleep
+
+import RPi.GPIO as GPIO
 
 # init configs
 GPIO.setmode(GPIO.BCM)
@@ -9,17 +10,27 @@ OUT_IR = 6
 EN_IR = 13
 GPIO.setup(OUT_IR, GPIO.IN)
 GPIO.setup(EN_IR, GPIO.OUT)
+
+# # LED (GPIO_LED)
+# GPIO_LED = 12
+# GPIO.setup(GPIO_LED, GPIO.OUT)
+
 print("IR Sensor Ready.....")
 print(" ")
 
 try: 
     while True:
-        if(GPIO.input(OUT_IR)):
+        if GPIO.input(OUT_IR):
             print("Obstacle detected")
+            # GPIO.output(GPIO_LED, GPIO.HIGH)  # Turn on the LED
         else:
             print("Obstacle clear")
+            # GPIO.output(GPIO_LED, GPIO.LOW)  # Turn off the LED
+        sleep(0.2)  # Delay for stability
 except KeyboardInterrupt:
     print("stopped")
+# finally:
+#     GPIO.cleanup()  # Clean up GPIO pins
 
 
     
