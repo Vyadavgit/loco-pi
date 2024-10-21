72926fd343e3d5b5c040f9da5f3d5d708b136b8c
Vyadavgit <70733761+Vyadavgit@users.noreply.github.com>
2024-10-20 19:50:37 -0500

    troubleshoot IR sensor

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import RPi.GPIO as GPIO
 import time
 
-sensor = 23
-buzzer = 18
+sensor = 16
+# buzzer = 18
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(sensor,GPIO.IN)
-GPIO.setup(buzzer,GPIO.OUT)
+# GPIO.setup(buzzer,GPIO.OUT)
 
-GPIO.output(buzzer,False)
+# GPIO.output(buzzer,False)
 print("IR Sensor Ready.....")
 print(" ")
 
 try: 
    while True:
       if GPIO.input(sensor):
-          GPIO.output(buzzer,True)
+        #   GPIO.output(buzzer,True)
           print("Object Detected")
           while GPIO.input(sensor):
               time.sleep(0.2)
       else:
-          GPIO.output(buzzer,False)
+          print("Obstacle cleared")
+        #   GPIO.output(buzzer,False)
 
 except KeyboardInterrupt:
     print("Stopped")
