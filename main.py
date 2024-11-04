eaea6c97edf454e86c9f6fe3f31e5ad5e4edfb95
Vyadavgit <70733761+Vyadavgit@users.noreply.github.com>
2024-11-03 18:55:52 -0600

    replace IR sensors

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 
-buzzer = 16
+IR_transmitter = 24
+IR_receiver = 25
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(buzzer,GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IR_transmitter, GPIO.OUT)
+GPIO.setup(IR_receiver, GPIO.IN)
 
-GPIO.output(buzzer,False)
-print("Buzzer Ready.....")
-GPIO.output(buzzer,True)
-print("stopped")
+GPIO.output(IR_transmitter, GPIO.LOW)
+print("IR initialisation.....",IR_transmitter)
+
+try:
+    while True:
+        GPIO.output(IR_transmitter,GPIO.HIGH)
+        print("Emitting IR...",IR_transmitter)
+        if(GPIO.input(IR_receiver)):
+            print("Receiving IR...",IR_receiver)
+except KeyboardInterrupt:
+    print("stopping...")
+    GPIO.output(IR_transmitter, GPIO.LOW)
+    print("Tramsmitting IR...",IR_transmitter)
+    print("Receiving IR...",IR_receiver)
+    print("stopped")
     
