7cdec59b101fd20d3424aa8df9b3d6ccc8aa6876
Vyadavgit <70733761+Vyadavgit@users.noreply.github.com>
2024-11-03 21:08:44 -0600

    troubleshoot IR receiver

--- main.py
+++ main.py
@@ -1,37 +1,21 @@
+import RPi.GPIO as GPIO
 import time
 
-import RPi.GPIO as GPIO
-
-IR_transmitter = 24
 IR_receiver = 25
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(IR_transmitter, GPIO.OUT)
 GPIO.setup(IR_receiver, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.output(IR_transmitter, GPIO.LOW)
-print("IR initialization complete. Transmitter on pin:", IR_transmitter)
+print("Monitoring IR receiver on pin:", IR_receiver)
 
 try:
     while True:
-        GPIO.output(IR_transmitter, GPIO.HIGH)
-        print("Emitting IR signal...")
-        # time.sleep(0.1)  # Transmit for 100ms
-        # GPIO.output(IR_transmitter, GPIO.LOW)
-        
-        if GPIO.input(IR_receiver) == GPIO.HIGH:
-            print("IR signal received on pin:", IR_receiver)
-        else:
-            print("No IR signal detected")
-        
-        GPIO.output(IR_transmitter, GPIO.LOW)
-        time.sleep(0.5)  # Wait for 500ms before the next transmission
+        state = GPIO.input(IR_receiver)
+        print("IR receiver state:", "HIGH" if state else "LOW")
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
     print("Stopping...")
 finally:
-    GPIO.output(IR_transmitter, GPIO.LOW)
-    GPIO.cleanup(IR_receiver)
     GPIO.cleanup()
     print("GPIO cleaned up. Program stopped.")
-    print()
