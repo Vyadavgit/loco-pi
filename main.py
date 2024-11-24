8ecbf4095586a3805ac633a422c9128d538e9fff
Vyadavgit <70733761+Vyadavgit@users.noreply.github.com>
2024-11-23 20:21:28 -0600

    working program1

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 
-IR_receiver = 12
-IR_emitter = 24
+IR_emitterPin = 24
+IR_receiverPin = 25
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(IR_receiver, GPIO.IN)  # No pull-up or pull-down specified
-GPIO.setup(IR_emitter, GPIO.OUT)  # Set IR emitter pin as output
+GPIO.setup(IR_emitterPin, GPIO.OUT)  # Set IR emitter pin as output
+GPIO.setup(IR_receiverPin, GPIO.IN)  # No pull-up or pull-down specified
 
-print("Monitoring IR receiver on pin:", IR_receiver)
+print("Monitoring IR receiver on pin:", IR_receiverPin)
 
 try:
     while True:
-        state = GPIO.input(IR_receiver)
+        state = GPIO.input(IR_receiverPin)
         print("IR receiver state:", "HIGH" if state else "LOW")
         
         # Control IR emitter based on receiver state
         if state:
-            GPIO.output(IR_emitter, GPIO.HIGH)  # Turn on IR emitter
+            GPIO.output(IR_emitterPin, GPIO.HIGH)  # Turn on IR emitter
             print("IR emitter state: ON")
         else:
-            GPIO.output(IR_emitter, GPIO.LOW)  # Turn off IR emitter
+            GPIO.output(IR_emitterPin, GPIO.LOW)  # Turn off IR emitter
             print("IR emitter state: OFF")
         
         # Detect object based on receiver state
