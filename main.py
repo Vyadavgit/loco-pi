46aefc9df06f62c2c03ea82425c3f9c1e1acb640
Vyadavgit <70733761+Vyadavgit@users.noreply.github.com>
2024-12-01 17:44:31 -0600

    if obstacle not detected for 50 seconds checked every 10 seconds then move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
-import datetime
+from datetime import datetime
 
 # init configs
 GPIO.setmode(GPIO.BCM)
@@ -39,93 +39,99 @@ GPIO.output(buzzer,False)
 print("IR receiver state: ",GPIO.input(IR_receiverPin))
 print("Buzzer & IRs initialized.....")
 obstacle = False
-last_buzz_time = datetime.datetime.now()
+last_buzz_time = datetime.now()
 init_buzz = True
+last_checked_interval = last_buzz_time
+no_of_times_obstacle_not_detected = 0
 
+def obstacle_detected():
+    global last_buzz_time, init_buzz, obstacle, no_of_times_obstacle_not_detected, last_checked_interval
+    # Control IR emitter based on receiver state
+    if state:
+        GPIO.output(IR_emitterPin, GPIO.HIGH)  # Turn on IR emitter
+        print("IR emitter state: ON")
+    else:
+        GPIO.output(IR_emitterPin, GPIO.LOW)  # Turn off IR emitter
+        print("IR emitter state: OFF")
+        
+    # Detect object based on receiver state
+    if state:
+        print("Object detected: NO")
+        if not obstacle_detected_in_last_five_checks():
+            obstacle = False
+    else:
+        print("Object detected: YES" + ", timestamp: "+ datetime.now().ctime)
+        # Check if 10 sec has passed since last buzz
+        current_time = datetime.now()
+        if (current_time - last_buzz_time).total_seconds() >= 10 or init_buzz:
+            GPIO.output(buzzer, True)  # Buzz
+            init_buzz = False
+            sleep(.5)  # Buzz for .5 second
+            GPIO.output(buzzer, False)  # Stop buzzing
+        last_buzz_time = current_time  # Update last buzz time
+        obstacle = True
+        no_of_times_obstacle_not_detected = 0
+        last_checked_interval = last_buzz_time
+    return obstacle
+
+def obstacle_detected_in_last_five_checks():
+    global no_of_times_obstacle_not_detected, last_checked_interval
+    margin_seconds = 5
+    if ((datetime.now() - last_checked_interval).total_seconds() >= 10) and not ((datetime.now() - last_buzz_time).total_seconds() > 50+margin_seconds):
+        no_of_times_obstacle_not_detected += 1
+        print("No. of times obstacle detected: "+no_of_times_obstacle_not_detected + " timestamp: "+ last_checked_interval.ctime)
+        last_checked_interval = datetime.now()
+        return True
+    else:
+        print("Obstacle not detected for last five checks")
+        return False
+    
+def move_forward():
+    # Motor 1 forward
+    GPIO.output(In1, GPIO.LOW)
+    GPIO.output(In2, GPIO.HIGH)
+    pwm1.ChangeDutyCycle(80)
+
+    # Motor 2 forward
+    GPIO.output(In4, GPIO.LOW)
+    GPIO.output(In3, GPIO.HIGH)
+    pwm2.ChangeDutyCycle(80)
+
+def move_backward():
+    # Motor 1 backward
+    GPIO.output(In1, GPIO.HIGH)
+    GPIO.output(In2, GPIO.LOW)
+    pwm1.ChangeDutyCycle(80)
+
+    # Motor 2 backward
+    GPIO.output(In4, GPIO.HIGH)
+    GPIO.output(In3, GPIO.LOW)
+    pwm2.ChangeDutyCycle(80)
+
+def stop():
+    print("Stopping motors...")
+    # Stop both motors
+    GPIO.output(In1, GPIO.LOW)
+    GPIO.output(In2, GPIO.LOW)
+    pwm1.ChangeDutyCycle(0)
+    GPIO.output(In3, GPIO.LOW)
+    GPIO.output(In4, GPIO.LOW)
+    pwm2.ChangeDutyCycle(0)
+    print("Stopped.")
+
+
+running = False   
 try:
     while True:
         state = GPIO.input(IR_receiverPin)
         print("IR receiver state:", "HIGH" if state else "LOW")
-
-        def obstacle_detected(last_buzz_time, init_buzz, obstacle):
-            # Control IR emitter based on receiver state
-            if state:
-                GPIO.output(IR_emitterPin, GPIO.HIGH)  # Turn on IR emitter
-                print("IR emitter state: ON")
-            else:
-                GPIO.output(IR_emitterPin, GPIO.LOW)  # Turn off IR emitter
-                print("IR emitter state: OFF")
-                
-            # Detect object based on receiver state
-            if state:
-                print("Object detected: NO")
-                # obstacle = False
-            else:
-                print("Object detected: YES")
-                # Check if 10 sec has passed since last buzz
-                current_time = datetime.datetime.now()
-                if (current_time - last_buzz_time).total_seconds() >= 10 or init_buzz:
-                    GPIO.output(buzzer, True)  # Buzz
-                    init_buzz = False
-                    sleep(.5)  # Buzz for .5 second
-                    GPIO.output(buzzer, False)  # Stop buzzing
-                last_buzz_time = current_time  # Update last buzz time
-                obstacle = True
-            return [last_buzz_time,init_buzz,obstacle]
-                
-        def move_forward():
-            # Motor 1 forward
-            GPIO.output(In1, GPIO.LOW)
-            GPIO.output(In2, GPIO.HIGH)
-            pwm1.ChangeDutyCycle(80)
-
-            # Motor 2 forward
-            GPIO.output(In4, GPIO.LOW)
-            GPIO.output(In3, GPIO.HIGH)
-            pwm2.ChangeDutyCycle(80)
-
-        def move_backward():
-            # Motor 1 backward
-            GPIO.output(In1, GPIO.HIGH)
-            GPIO.output(In2, GPIO.LOW)
-            pwm1.ChangeDutyCycle(80)
-
-            # Motor 2 backward
-            GPIO.output(In4, GPIO.HIGH)
-            GPIO.output(In3, GPIO.LOW)
-            pwm2.ChangeDutyCycle(80)
-        
-        def stop():
-            print("Stopping motors...")
-            # Stop both motors
-            GPIO.output(In1, GPIO.LOW)
-            GPIO.output(In2, GPIO.LOW)
-            pwm1.ChangeDutyCycle(0)
-            GPIO.output(In3, GPIO.LOW)
-            GPIO.output(In4, GPIO.LOW)
-            pwm2.ChangeDutyCycle(0)
-            print("Stopped.")
-        
-        # def recheck_obstacle():
-        #     obstacle = False
-        #     obstacle_detected()
-        #     if not obstacle:
-        #         continue
-        #     else:
-        #         sleep(3)
-        #         recheck_obstacle()
-
-        ret = obstacle_detected(last_buzz_time,init_buzz,obstacle)
-        last_buzz_time = ret[0]
-        init_buzz = ret[1]
-        obstacle = ret[2]
-        # move_forward() if not obstacle else stop()
-        if obstacle:
+        obstacle_detected()
+        if obstacle and no_of_times_obstacle_not_detected < 5 and running:
             stop()
+            running = False
         else:
+            running = True
             move_forward()
-        # move_backward()
-        
 except KeyboardInterrupt:
     print("Stopping...")
 # finally:
